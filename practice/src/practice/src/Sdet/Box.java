5b789dae393f5d1bd6665b40bd7d3532fb788b4f
Rajat Shandilya <Rajat Shandilya@RajatShandilya>
2018-06-19 17:02:28 +0530

    first commit

---
+++ practice/src/practice/src/Sdet/Box.java
@@ -0,0 +1,21 @@
+package practice.src.Sdet;
+
+public class Box {
+	private int length,breadth , height;
+	public void setDiamention(int l,int b,int h) {
+		length=l;
+		breadth=b;
+		height=h;
+	}
+	public void showDiamention() {
+	System.out.println("L="+length);
+	//System.out.println("B="+breadth);
+	//System.out.println("H="+height);
+	
+	
+	
+	
+	}
+	
+
+}
---
+++ practice/src/practice/src/Sdet/ExampleBox.java
@@ -0,0 +1,14 @@
+package practice.src.Sdet;
+
+public class ExampleBox {
+
+	public static void main(String[] args) {
+		Box b=new Box();
+		b.setDiamention(12, 10, 5);
+		b.showDiamention();
+		Box box = new Box();
+		b.showDiamention();
+
+	}
+
+}
